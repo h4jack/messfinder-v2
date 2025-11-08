7f9ef9548ad2e7fa4d1acb4e30a499d664222325
h4jack <susantamandi.dev@gmail.com>
2025-11-08 12:44:42 +0530

    added new package and initiated the updating of homesearch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import { Header } from "@/components/layout";
+import FloatingSearch from "@/components/section/FloatingSearch";
+import HomeSearch from "@/components/section/HomeSearch";
+
 export default function Home() {
   return (
-    <h1 className="text-center">Hello World</h1>
+    <>
+      <div className="bg-gradient-to-br from-teal-50 to-white min-h-screen">
+        <Header />
+        <HomeSearch />
+      </div>
+    </>
   );
 }
